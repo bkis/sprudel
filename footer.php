7f971ff68c842fa9a95e1a07802bfab4652a1e00
bkis <kiss@mailbox.org>
2019-11-03 15:59:28 +0100

    new footer design

--- footer.php
+++ footer.php
@@ -1,11 +1,13 @@
 
-<?php
-
-	if (SPR_SHOW_FOOTER_LINK == 1){
-		echo "<div id='footer'>Sprudel open source scheduling polls. Visit on <a href='https://github.com/bkis/sprudel' target='_blank'>GitHub</a>.</div>";
-	}
-
-?>
+<?php if (SPR_SHOW_FOOTER_LINK == 1){ ?>
+	<div id="footer">
+		<em>Sprudel</em> open source scheduling polls.
+		<a href="https://github.com/bkis/sprudel" target='_blank'>
+			<em>Visit on GitHub</em>
+			<img src="<?php echo SPR_BASE_URL ?>img/icon-github.png" alt=""/>
+		</a>
+	</div>
+<?php } ?>
 
 </body>
 </html>
